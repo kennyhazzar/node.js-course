6c58df3204f20c69af4f1030fc4f75e741c83cac
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2022-05-24 21:25:28 +0700

    пример выполнения многопоточной библиотеки crypto в node.js

---
+++ crypto-example.js
@@ -0,0 +1,25 @@
+﻿const crypto = require('crypto')
+
+const start = Date.now()
+
+crypto.pbkdf2('123ttt', '5', 100000, 64, 'sha512', () => {
+    console.log('1 end', Date.now() - start)
+})
+
+crypto.pbkdf2('123ttt', '5', 100000, 64, 'sha512', () => {
+    console.log('2 end', Date.now() - start)
+})
+
+crypto.pbkdf2('123ttt', '5', 100000, 64, 'sha512', () => {
+    console.log('3 end', Date.now() - start)
+})
+
+crypto.pbkdf2('123ttt', '5', 100000, 64, 'sha512', () => {
+    console.log('4 end', Date.now() - start)
+})
+
+crypto.pbkdf2('123ttt', '5', 100000, 64, 'sha512', () => {
+    console.log('5 end', Date.now() - start)
+})
+
+console.log(start)
