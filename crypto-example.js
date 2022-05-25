662ae75b86dfffdbe58c55c2cf10f947ab9ff0ee
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2022-05-25 10:31:45 +0700

    изменено количество итераций

--- crypto-example.js
+++ crypto-example.js
@@ -2,23 +2,23 @@
 
 const start = Date.now()
 
-crypto.pbkdf2('123ttt', '5', 100000, 64, 'sha512', () => {
+crypto.pbkdf2('123ttt', '5', 1000000, 64, 'sha512', () => {
     console.log('1 end', Date.now() - start)
 })
 
-crypto.pbkdf2('123ttt', '5', 100000, 64, 'sha512', () => {
+crypto.pbkdf2('123ttt', '5', 1000000, 64, 'sha512', () => {
     console.log('2 end', Date.now() - start)
 })
 
-crypto.pbkdf2('123ttt', '5', 100000, 64, 'sha512', () => {
+crypto.pbkdf2('123ttt', '5', 1000000, 64, 'sha512', () => {
     console.log('3 end', Date.now() - start)
 })
 
-crypto.pbkdf2('123ttt', '5', 100000, 64, 'sha512', () => {
+crypto.pbkdf2('123ttt', '5', 1000000, 64, 'sha512', () => {
     console.log('4 end', Date.now() - start)
 })
 
-crypto.pbkdf2('123ttt', '5', 100000, 64, 'sha512', () => {
+crypto.pbkdf2('123ttt', '5', 1000000, 64, 'sha512', () => {
     console.log('5 end', Date.now() - start)
 })
 
